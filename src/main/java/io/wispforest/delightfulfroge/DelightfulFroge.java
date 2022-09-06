7e9fae3ca045688b54e1b862931d91f59ddfb788
glisco <glisco03@web.de>
2022-09-06 23:34:05 +0200

    add frogelight :), introduce froge patting :)

--- src/main/java/io/wispforest/delightfulfroge/DelightfulFroge.java
+++ src/main/java/io/wispforest/delightfulfroge/DelightfulFroge.java
@@ -3,7 +3,22 @@ package io.wispforest.delightfulfroge;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.PillarBlock;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.FrogVariant;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.condition.DamageSourcePropertiesLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.predicate.entity.DamageSourcePredicate;
+import net.minecraft.predicate.entity.EntityPredicate;
+import net.minecraft.predicate.entity.TypeSpecificPredicate;
 import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -13,6 +28,7 @@ import net.minecraft.world.biome.Biome;
 public class DelightfulFroge implements ModInitializer {
 
     public static final FrogVariant FROGE = new FrogVariant(DelightfulFroge.id("textures/entity/frog/froge.png"));
+    public static final Block FROGELIGHT = new PillarBlock(FabricBlockSettings.copyOf(Blocks.OCHRE_FROGLIGHT));
 
     public static final TagKey<Biome> FROGE_PLACES = TagKey.of(Registry.BIOME_KEY, DelightfulFroge.id("froge_places"));
 
@@ -29,6 +45,27 @@ public class DelightfulFroge implements ModInitializer {
                 DelightfulFroge.id("froge"),
                 FROGE
         );
+
+        Registry.register(
+                Registry.BLOCK,
+                DelightfulFroge.id("frogelight"),
+                FROGELIGHT
+        );
+        Registry.register(
+                Registry.ITEM,
+                DelightfulFroge.id("frogelight"),
+                new BlockItem(FROGELIGHT, new Item.Settings().group(ItemGroup.DECORATIONS))
+        );
+
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if (!id.equals(EntityType.MAGMA_CUBE.getLootTableId())) return;
+            tableBuilder.pool(
+                    LootPool.builder().with(ItemEntry.builder(FROGELIGHT).conditionally(
+                            DamageSourcePropertiesLootCondition.builder(DamageSourcePredicate.Builder.create()
+                                    .sourceEntity(EntityPredicate.Builder.create().typeSpecific(TypeSpecificPredicate.frog(FROGE))))
+                    )).build()
+            );
+        });
     }
 
     public static Identifier id(String path) {
--- src/main/java/io/wispforest/delightfulfroge/mixin/FrogEntityMixin.java
+++ src/main/java/io/wispforest/delightfulfroge/mixin/FrogEntityMixin.java
@@ -1,17 +1,23 @@
 package io.wispforest.delightfulfroge.mixin;
 
 import io.wispforest.delightfulfroge.DelightfulFroge;
+import net.minecraft.entity.AnimationState;
 import net.minecraft.entity.EntityData;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.FrogEntity;
 import net.minecraft.entity.passive.FrogVariant;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,14 +25,39 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FrogEntity.class)
-public abstract class FrogEntityMixin extends LivingEntity {
+public abstract class FrogEntityMixin extends MobEntity {
 
-    protected FrogEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+    protected FrogEntityMixin(EntityType<? extends MobEntity> entityType, World world) {
         super(entityType, world);
     }
 
     @Shadow
     public abstract void setVariant(FrogVariant variant);
+
+    @Shadow
+    public abstract FrogVariant getVariant();
+
+    @Shadow
+    @Final
+    public AnimationState usingTongueAnimationState;
+
+    @Override
+    protected ActionResult interactMob(PlayerEntity player, Hand hand) {
+        if (!player.isSneaking() || this.getVariant() != DelightfulFroge.FROGE) return ActionResult.PASS;
+        if (this.world.isClient) {
+            for (int i = 0; i < 5; i++) {
+                double x = this.getX() + (this.random.nextDouble() - .5);
+                double y = this.getY() + (this.random.nextDouble() - .5) + .25;
+                double z = this.getZ() + (this.random.nextDouble() - .5);
+
+                this.world.addParticle(ParticleTypes.HEART, x, y, z, 0, 0, 0);
+                this.world.addParticle(ParticleTypes.WAX_ON, x, y + .4, z, this.random.nextDouble() * 2.5, this.random.nextDouble() * 2.5, this.random.nextDouble() * 2.5);
+            }
+
+            this.usingTongueAnimationState.start(this.age);
+        }
+        return ActionResult.SUCCESS;
+    }
 
     @Inject(method = "initialize", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/passive/FrogBrain;coolDownLongJump(Lnet/minecraft/entity/passive/FrogEntity;Lnet/minecraft/util/math/random/Random;)V"))
     private void injectFroge(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, EntityData entityData, NbtCompound entityNbt, CallbackInfoReturnable<EntityData> cir) {
